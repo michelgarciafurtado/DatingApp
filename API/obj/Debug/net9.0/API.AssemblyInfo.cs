1b753b3ecf30e95490e3a060798e1a97266b6057
michelgarciafurtado <michelgarciafurtado@gmail.com>
2025-09-17 07:11:49 -0300

    update assemblies

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df621c18ec3587454581a7facf9455bc02d27dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071235793a65f341096957c5a98fc99cda2d9cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
